f8614f3eb75e147a3b8accd6903cca8974ddf686
FelipeDNL <felipednlled@gmail.com>
2025-01-29 23:29:08 -0300

    Update hook to use the new Octokit API

--- src/hooks/usarGithubOctokitRepos.jsx
+++ src/hooks/usarGithubOctokitRepos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import { Octokit } from '@octokit/rest'
 
 function usarGithubOctokitRepos(username, token) {
@@ -7,40 +7,32 @@ function usarGithubOctokitRepos(username, token) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-
-    // autenticado com token de acesso pessoal
-    // não é necessário mas aumenta o limite de requisições
     const octokit = new Octokit({
       auth: import.meta.env.VITE_GITHUB_TOKEN,
     });
 
-    // função assíncrona para buscar os repositórios
     const fetchRepos = async () => {
       try {
-
-        // busca os repositórios do usuário [username]
         const response = await octokit.repos.listForUser({
-          username
+          username: username,
+          per_page: 100 // Número máximo de repositórios por página
+        });
+    
+        const repos = response.data;
+        repos.forEach(repo => {
+          console.log(repo.name);
         });
 
-        // mapeia os nomes dos repositórios
-        const repoNames = response.data.map(repo => repo.name);
-
-        // atualiza o estado com os nomes dos repositórios
-        setRepos(repoNames);
-
-      } catch (err) {
-        setError(err);
-
-      } finally { // finally é executado independente de erro ou sucesso
+        setRepos(repos);
+      } catch (error) {
+        setError(error);
+      } finally {
         setLoading(false);
       }
     };
 
-    // chama a função para buscar os repositórios
     fetchRepos();
-    
-  }, [username, token]);// executa o efeito sempre que o username ou token mudar
+  }, [username, token]);
 
   return { repos, loading, error };
 }
